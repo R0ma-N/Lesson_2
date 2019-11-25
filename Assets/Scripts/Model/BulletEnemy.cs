594ce97cb4a074b03fdb08f09180ec7d5ce9669f
R0ma-N <x6wert@gmail.com>
2019-11-26 01:03:42 +0300

    Lesson4

---
+++ Assets/Scripts/Model/BulletEnemy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Geekbrains
+{
+	//ПУЛЯ. ЧАСТНОСТЬ ОТ ТИПОВ СНАРЯДА
+    public sealed class BulletEnemy : Ammunition
+	{
+		
+        private void OnCollisionEnter(Collision collision)
+		{
+
+            var tempObj = collision.gameObject.GetComponent<ISetDamage>();
+
+
+            if (tempObj != null)
+			{
+				//сталкивается с объектом и передает инфу об уроне и направлении
+                tempObj.SetDamage(new InfoCollision(_curDamage, Rigidbody.velocity));
+			}
+
+            DestroyAmmunition();
+		}
+	}
+}
